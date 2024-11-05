1fd6cb9b4be213e2d73bc862a155ffd38b69c435
AntonioHugo21 <antonio.pierre@aluno.ce.gov.br>
2024-11-05 20:17:58 -0300

    Vetor de idades

---
+++ atividade37.py
@@ -0,0 +1,7 @@
+vetor_de_idades = []
+
+for cont in range(0,20):
+    vetor_de_idades.append(int(input('Digite a sua idade: ')))
+
+print(f'A maior idade é {max(vetor_de_idades)}')
+print(f'A menor idade é {min(vetor_de_idades)}')
